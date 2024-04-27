49589fbccecc50d5cd984b99c39ae20c93e891b7
Yehor Lesnevych <egor.lesnev@ukr.net>
2024-04-27 22:33:47 +0300

    Update site.ts

--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-	name: "Next.js + NextUI",
-	description: "Make beautiful websites regardless of your design experience.",
+	name: "Pizzeria Shon",
+	description: "Enjoy the best meals!",
 	navItems: [
 		{
 			label: "Home",
@@ -60,10 +60,10 @@ export const siteConfig = {
 		},
 	],
 	links: {
-		github: "https://github.com/nextui-org/nextui",
-		twitter: "https://twitter.com/getnextui",
+		github: "https://github.com/YehorLesnev/PizzeriaAPI",
+		twitter: "https://twitter.com/",
 		docs: "https://nextui.org",
-		discord: "https://discord.gg/9b6yyZKmH4",
-    sponsor: "https://patreon.com/jrgarciadev"
+		discord: "https://discord.gg/",
+    sponsor: "https://github.com/YehorLesnev/PizzeriaAPI"
 	},
 };
